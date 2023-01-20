2944a3150eb0b72b7821d0b001b579dd8773e313
JoseSom <joseluissom004@gmail.com>
2023-01-20 14:44:03 -0600

    Se agregaron los ejercicios como se solicita

---
+++ src/Coche.java
@@ -0,0 +1,11 @@
+public class Coche {
+    private int puertas;
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Ejercicio 1
+        /*
+        Crear una función con tres parámetros que sean números que se suman entre sí.
+        Llamar a la función en el main y darle valores.
+        */
+
+        int resultadoSumaTresNumeros = sumaTresNumeros(10,5,95);
+        System.out.println("Suma total de 3 numeros : " + resultadoSumaTresNumeros);
+
+
+        //Ejercicio 2
+        /*Crear una clase coche.
+        Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+        Una función que incremente el número de puertas que tiene el coche.
+        Crear un objeto miCoche en el main y añadirle una puerta.
+        Mostrar el número de puertas que tiene el objeto.*/
+
+        Coche miCoche = new Coche();
+        System.out.println("Numero de puertas : " + miCoche.getPuertas());
+        miCoche.setPuertas(4);
+        System.out.println("Numero de puertas : " + miCoche.getPuertas());
+
+    }
+
+    private static int sumaTresNumeros(int a, int b, int c) {
+        return a+b+c;
     }
 }
